cd36c0a9e80e70aba99c995317051742b521cfc8
pulsation <philippe.sam-long@laposte.net>
2013-07-30 17:29:30 +0200

    Bind event to the OnClickListener

--- src/eu/pulsation/alarmsandbox/authentication/AuthenticatorAccountActivity.scala
+++ src/eu/pulsation/alarmsandbox/authentication/AuthenticatorAccountActivity.scala
@@ -1,14 +1,16 @@
 package eu.pulsation.alarmsandbox
 
-import android.accounts.AccountAuthenticatorActivity
+import android.accounts.{AccountAuthenticatorActivity, AccountManager}
 import android.os.Bundle
 import android.app.Activity
-import android.widget.Button
+import android.widget.{Button, EditText}
 import android.view.View.OnClickListener
 import android.view.View
+import android.accounts.Account
 
 class AuthenticatorAccountActivity extends AccountAuthenticatorActivity {
   val AccountType : String = "eu.pulsation.alarmsandbox.couchdb"
+  val that = this
 
   override def onCreate(savedInstanceState: Bundle) {
 
@@ -19,15 +21,36 @@ class AuthenticatorAccountActivity extends AccountAuthenticatorActivity {
       }
     }
 
+    lazy val loginField = {
+      this.findViewById(R.id.LoginField) match {
+          case txt : EditText => txt
+          case _ => throw new ClassCastException
+      }
+    }
+
+    lazy val passwordField = {
+      this.findViewById(R.id.PasswordField) match {
+          case txt : EditText => txt
+          case _ => throw new ClassCastException
+      }
+    }
+
+    lazy val login = loginField.getText().toString()
+    lazy val password = passwordField.getText().toString()
+
+    val that = this
+
     super.onCreate(savedInstanceState)
     setContentView(R.layout.account)
 
     // Bind buttons
     proceedBtn.setOnClickListener(new OnClickListener() {
       def onClick(v: View) {
-        // TODO
+        val account : Account = new Account(login, AccountType)
+
+        AccountManager.get(that).addAccountExplicitly(account, password, null)
+        that.finish()
       }
     })
   }
-
 }
