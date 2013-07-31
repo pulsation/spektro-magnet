2887c0542178fdabfae39e93d1b664e712fcc639
pulsation <philippe.sam-long@laposte.net>
2013-07-31 17:40:50 +0200

    Begin sync provider implementation

---
+++ src/eu/pulsation/alarmsandbox/sync/AlarmSandboxSyncAdapterService.scala
@@ -0,0 +1,20 @@
+package eu.pulsation.alarmsandbox
+
+/**
+ * Implemented from example http://www.c99.org/2010/01/23/writing-an-android-sync-provider-part-2/
+ */
+
+/*
+class AlarmSandboxSyncAdapterService extends Service {
+  private static class SyncAdapterImpl(context: Context, autoInitialize: boolean) extends AbstractThreadedSyncAdapter(context: Context,
+    autoInitialize: boolean) {
+    
+    this(context) = this(context, true)
+
+    def onPerformSync(account: Account, extras: Bundle, authority: String, provider: ContentProviderClient, syncResult: SyncResult) {
+      AlarmSandboxSyncAdapterService.performSync()
+    }
+
+  }
+}
+*/
