e626dbdb6ba3ee917c6f34bb171a8029b5a8859e
pulsation <philippe.sam-long@laposte.net>
2013-08-06 17:47:06 +0200

    Convert java dummy content provider to scala

---
+++ src/eu/pulsation/alarmsandbox/AlarmSandboxContentProvider.scala
@@ -0,0 +1,35 @@
+package eu.pulsation.alarmsandbox
+
+import android.content.ContentProvider
+import android.content.ContentValues
+import android.net.Uri
+import android.database.Cursor
+
+class AlarmSandboxContentProvider extends ContentProvider {
+
+    def delete(uri: Uri, selection: String, selectionArgs: Array[String]) : Int = {
+      0
+    }
+
+    def getType(uri: Uri) : String = {
+      null
+    }
+
+    def insert(uri : Uri, values: ContentValues) : Uri = {
+        null
+    }
+
+    def onCreate : Boolean = {
+        false
+    }
+
+     def query(uri: Uri, projection: Array[String], selection: String,
+               selectionArgs : Array[String] , sortOrder : String) : Cursor = {
+        null
+    }
+
+    def update(uri: Uri, values: ContentValues, selection: String,
+                   selectionArgs : Array[String] ) : Int = {
+        0
+    }
+}
